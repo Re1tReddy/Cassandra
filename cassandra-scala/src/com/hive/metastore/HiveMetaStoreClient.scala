d77b8f2abe2c440c51785040d5c1786de792aee8
Revanth Reddy <revanthkumar95@gmail.com>
2017-07-20 19:00:20 +0530

    Connecting To HiveMetaStore using HiveMetaStoreClient API

--- cassandra-scala/src/com/hive/metastore/HiveMetaStoreClient.scala
+++ cassandra-scala/src/com/hive/metastore/HiveMetaStoreClient.scala
@@ -1,24 +1,21 @@
+package com.hive.metastore
 
-
+import org.apache.hadoop.hive.conf.HiveConf
 import com.datastax.driver.core.Cluster
 import com.datastax.driver.core.querybuilder.QueryBuilder
-import com.datastax.driver.core.Row
-import org.apache.hadoop.hive.conf.HiveConf
+import com.hive.metastore.HiveMetaStoreClient
 import org.apache.hadoop.hive.metastore.HiveMetaStoreClient
-import org.apache.hadoop.hive.metastore.api.Partition
-import org.apache.hadoop.hive.metastore.api.StorageDescriptor
-object Test extends App {
+
+object HiveMetaStoreClient extends App {
 
   val hiveConf = new HiveConf(getClass)
-  // hiveConf.set("hive.metastore.local", "true");
   hiveConf.setVar(HiveConf.ConfVars.METASTOREURIS, "thrift://127.0.0.1:9083");
   val hiveClient = new HiveMetaStoreClient(hiveConf);
 
-  println(hiveClient.getAllDatabases().size());
+  println("No of databases :" + hiveClient.getAllDatabases().size());
 
   val cluster = Cluster.builder().addContactPoint("127.0.0.1").build()
   val masterSession = cluster.connect("dev")
-  //val tidQuery = masterSession.prepare(s"SELECT empid FROM emp")
 
   val statement = QueryBuilder.select().all().from("dev", "emp")
   val requestQuery = masterSession.execute(statement);
